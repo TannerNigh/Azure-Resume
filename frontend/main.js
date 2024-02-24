c8173416410073a4777c748601de2bbea3cf2674
Rustano <rustano.local>
2024-02-24 14:58:56 -0800

    Updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', (even) => {
+
+    getVisitCount();
+
+})
+
+const functionAPi   = '';
+
+const getVisitCount = () => {
+
+    let count = 30;
+    fetch(functionAPi).then(response => {
+
+        return response.json()
+    }).then(response => {
+
+        console.log("Website called function API.")
+        count = response.count;
+        document.getElementById("counter").innerText = count; 
+
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count; 
+
+}
